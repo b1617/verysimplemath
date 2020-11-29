3aff654647370cd371b64700648660334c2d62eb
Dhinesh <sattia.dhinesh@gmail.com>
2020-11-29 16:08:00 +0100

    completed test

--- src/test/math.test.js
+++ src/test/math.test.js
@@ -16,6 +16,14 @@ describe('add', () => {
   it('should add any number of args', () => {
     expect(math.add(1, 2, 3, 4, 5, 6)).toBe(21);
   });
+
+  it('should add negative numbers', () => {
+    expect(math.add(-1, -2, -3)).toBe(-6);
+  });
+
+  it('should add negative and postive numbers', () => {
+    expect(math.add(-1, 4, -2, 5, 6, -3)).toBe(9);
+  });
 });
 
 describe('substract', () => {
@@ -25,20 +33,23 @@ describe('substract', () => {
   it('should return a sole number', () => {
     expect(math.substract(1)).toBe(1);
   });
-  it('should substract two number', () => {
+  it('should substract two numbers', () => {
     expect(math.substract(1, 1)).toBe(0);
   });
-  it('should substract two number', () => {
+  it('should substract two numbers', () => {
     expect(math.substract(2, 1)).toBe(1);
   });
-  it('should substract three number', () => {
+  it('should substract three numbers', () => {
     expect(math.substract(0, 1, 1)).toBe(-2);
   });
   it('should substract any number of args', () => {
     expect(math.substract(2, 1, 1, 2)).toBe(-2);
   });
-  it('should substract negative number ', () => {
+  it('should substract negative numbers ', () => {
     expect(math.substract(-4, -2)).toBe(-2);
+  });
+  it('should substract negative and positive numbers ', () => {
+    expect(math.substract(-4, 5, -2, 7)).toBe(-14);
   });
 });
 
@@ -51,11 +62,23 @@ describe('multiply', () => {
     expect(math.multiply(5)).toBe(5);
   });
 
-  it('should multiply 2 number', () => {
+  it('should multiply 2 numbers', () => {
     expect(math.multiply(1, 2)).toBe(2);
   });
-  it('should multiply n number', () => {
+  it('should multiply n numbers', () => {
     expect(math.multiply(1, 2, 3)).toBe(6);
+  });
+
+  it('shoud multiply negative numbers', () => {
+    expect(math.multiply(-8, -4)).toBe(32);
+  });
+
+  it('shoud multiply negative and positive numbers', () => {
+    expect(math.multiply(-8, 4)).toBe(-32);
+  });
+
+  it('shoud multiply numbers', () => {
+    expect(math.multiply(-8, -4, 2, -5, 5)).toBe(-1600);
   });
 });
 
@@ -68,7 +91,7 @@ describe('divide', () => {
     expect(math.divide(1)).toBe(1);
   });
 
-  it('should divide two number', () => {
+  it('should divide two numbers', () => {
     expect(math.divide(2, 2)).toBe(1);
   });
 
@@ -82,5 +105,13 @@ describe('divide', () => {
 
   it('shoud return 0 if args contains 0', () => {
     expect(math.divide(5, 6, 3, 0, 4)).toBe(0);
+  });
+
+  it('shoud divide negative numbers', () => {
+    expect(math.divide(-8, -4)).toBe(2);
+  });
+
+  it('shoud divide negative  and positive numbers', () => {
+    expect(math.divide(-8, 4)).toBe(-2);
   });
 });
